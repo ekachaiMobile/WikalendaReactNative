1abb137542279bf297e7c6199696b9b93bfe6e4d
ekachai <ekachai@23perspective.com>
2017-10-02 12:37:56 +0700

    tab

--- index.ios.js
+++ index.ios.js
@@ -8,5 +8,5 @@ import { AppRegistry } from "react-native";
 import WikalendaNativeBase from "./js/App";
 // import Home from "./js/screens/Home";
 // import AppNavigator from "./js/App";
-
+// import Detail from "./js/screens/Detail";
 AppRegistry.registerComponent('WikalendaNativeBase', () => WikalendaNativeBase);
--- js/screens/Home/index.js
+++ js/screens/Home/index.js
@@ -9,42 +9,50 @@ import {
   Button,
   Icon
 } from 'react-native';
+import {
+  Container,
+  ScrollableTab,
+  Tabs,
+  Tab
+} from "native-base";
+
+import Detail from "./../Detail";
 
 export default class Home extends Component {
   render() {
     return (
+      <Container>
+      <Tabs renderTabBar={() => <ScrollableTab />}>
+        <Tab heading="Tab1">
+          <Detail />
+        </Tab>
+        <Tab heading="Tab2">
+          <Detail />
+        </Tab>
+        <Tab heading="Tab3">
+          <Detail />
+        </Tab>
+        <Tab heading="Tab4">
+          <Detail />
+        </Tab>
+        <Tab heading="Tab5">
+          <Detail />
+        </Tab>
+      </Tabs>
+    </Container>
 
-      //   <Header>
-      //   <Left>
-      //     <Button transparent onPress={() => this.props.navigation.goBack()}>
-      //       <Icon name="arrow-back" />
-      //     </Button>
-      //   </Left>
-      //   <Body>
-      //     <Title>Welcome!</Title>
-      //   </Body>
-      //   <Right>
-      //     <Button transparent onPress={() => alert("We love you too!")}>
-      //       <Icon name="heart" />
-      //     </Button>
-      //   </Right>
-      // </Header>
-
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native! This is home
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-        <Image
-          source={require('./logo.png')}
-        />
-      </View>
+      // <View style={styles.container}>
+      //   <Text style={styles.welcome}>
+      //     Welcome to React Native! This is home
+      //   </Text>
+      //   <Text style={styles.instructions}>
+      //     To get started, edit index.ios.js
+      //   </Text>
+      //   <Text style={styles.instructions}>
+      //     Press Cmd+R to reload,{'\n'}
+      //     Cmd+D or shake for dev menu
+      //   </Text>
+      // </View>
     );
   }
 }
